c48c74b409a98d914ae8538013dcd84a56b4d541
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2024-09-30 14:30:17 +0700

    fetching data from backend using api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,53 @@
 'use client'
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+type Product = {
+  id : number;
+  name : string;
+  price : number;
+  status : string;
+}
+const [products, setProducts] = useState<Product[]>([]);
 useEffect(() => {
-  function getdata(){
-    const getdata = axios.get('http://127.0.0.1:8000/home').then(
-      function(response){
-        console.log(response)
-      }
-    ).catch(
-      function(error){
-        console.log(error)
-      }
-    )
+  const getData = async () =>{
+    try{
+      const response = await axios.get('http://127.0.0.1:8000/products');
+      setProducts(response.data);
+    }
+    catch (error){
+      console.log(error);
+    }
   }
-  getdata();
+  getData();
 },[]);
+
+function RupiahFormat(num: number){
+  return 'Rp.' + num.toLocaleString(undefined,{minimumFractionDigits: 2})
+}
   return (
     <div>
       <h1>List barang :</h1>
       <div>
-        <ul>
-          <li>
-            
-          </li>
-        </ul>
+            {
+              products.map((product)=>(
+                <ul>
+                  <li>
+                    {product.id}
+                  </li>
+                  <li>
+                    {product.name}
+                  </li>
+                  <li>
+                    {RupiahFormat(product.price)}
+                  </li>
+                  <li>
+                    {product.status}
+                  </li>
+                </ul>
+              ))
+            }
       </div>
     </div>
   );
